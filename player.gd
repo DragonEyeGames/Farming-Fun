b544f5e605a4468cab09ce0bbcc067467180d5b8
Taxalottle Syndicate <bthole41@gmail.com>
2025-12-20 21:59:34 -0500

    Setup + Art
    Imported in quite a bit of art and got the setup of the player working! There is some fancy backgrounds and a windmill tihng!

---
+++ player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var count :=0.0
+var toDisplay="right"
+
+func _process(delta: float) -> void:
+	#if(Input.is_action_just_pressed("Interact")):
+	count+=delta
+	velocity = Input.get_vector("Left", "Right", "Up", "Down")*80
+	move_and_slide()
+	if(velocity.x<0):
+		$Body.scale.x=-1
+	elif(velocity.x>0):
+		$Body.scale.x=1
+		
+	if(count>.14 and not velocity==Vector2.ZERO):
+		count=0
+		for child in $Body.get_children():
+			child.animation=toDisplay
+			if(child.frame>=5):
+				child.frame=0
+			else:
+				child.frame+=1
+	elif(velocity==Vector2.ZERO):
+		for child in $Body.get_children():
+			child.frame=0
