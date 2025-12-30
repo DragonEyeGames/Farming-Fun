0b885047be836ed4601f4ad43dbfba24ad74914a
Taxalottle Syndicate <bthole41@gmail.com>
2025-12-30 17:30:31 -0500

    House
    I put in the sprites and design for a house! Functionality to come later... This includes a bed to skip the time!

--- Scripts/inventory_slot.gd
+++ Scripts/inventory_slot.gd
@@ -2,12 +2,16 @@ extends Control
 
 @export var index:=1
 @export var selectable:=true
+@export var shopSlot:=false
+var itemName=""
+var selected=false
 
 func _ready() -> void:
 	await get_tree().process_frame
 	$RichTextLabel.text=str(index+1)
 
 func _process(_delta: float) -> void:
+	itemName=""
 	$InventoryOutline.visible=GameManager.playerSelected==index and selectable
 	var keys := GameManager.playerInventory.keys()
 	
@@ -17,11 +21,24 @@ func _process(_delta: float) -> void:
 	if index < keys.size():
 		var item = keys[index]
 		if GameManager.playerInventory[item] > 0:
-			var itemName = GameManager.inventoryItem.keys()[item]
+			itemName = GameManager.inventoryItem.keys()[item]
 			$Items.get_node(itemName).visible = true
 			var count := GameManager.playerInventory[item]
 			$Value.text = str(count)
 	else:
 		$Value.text=""
 		
-		
+	if(selected and shopSlot and Input.is_action_just_pressed("Interact")):
+		pass
+
+
+func _on_color_rect_mouse_entered() -> void:
+	if(itemName!=""):
+		$Popup.visible=true
+		$Popup/Title.text=itemName
+		$Popup/Title.text=$Popup/Title.text.replace("_", " ")
+		selected=true
+
+func _on_color_rect_mouse_exited() -> void:
+	$Popup.visible=false
+	selected=false
