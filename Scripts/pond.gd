d3f74de71db81e26f9e34ab8cf6460390c9b35f4
Taxalottle Syndicate <bthole41@gmail.com>
2026-01-12 18:02:10 -0500

    Watering can refiller
    I made it so that the watering can can be refilled if you go to the pond and interact with it! Infinite water for life!

--- Scripts/pond.gd
+++ Scripts/pond.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var tick:=0.0
 
+var playerEntered:=false
+
 func _process(delta: float) -> void:
 	tick+=delta
 	if(tick>=10):
@@ -11,3 +13,16 @@ func _process(delta: float) -> void:
 		fish.play("splash-" + str(randi_range(1, 2)))
 		fish.position=Vector2(randi_range(-4, 38), randi_range(13, 44))
 		fish.flip_h=randi_range(1, 2)==1
+	
+func interact():
+	if(GameManager.itemSelected("watering")):
+		GameManager.selectedItem.amount=GameManager.limited[GameManager.selectedItem.item]
+
+
+func _on_refill_area_body_entered(body: Node2D) -> void:
+	body.interacting.append(self)
+
+
+func _on_refill_area_body_exited(body: Node2D) -> void:
+	if(self in body.interacting):
+		body.interacting.erase(self)
