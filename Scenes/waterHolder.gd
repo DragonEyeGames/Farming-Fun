71528a8a349412161424d2c90e64b2e7537a31a7
Taxalottle Syndicate <bthole41@gmail.com>
2026-01-24 19:22:27 -0500

    Food and water holder for the chicken
    Started working on food and water for the hcickens

--- Scenes/waterHolder.gd
+++ Scenes/waterHolder.gd
@@ -1,1 +1,8 @@
-extends Sprite2D
+extends Node2D
+
+@export var maxWater=5.0
+@export var refillMethod: GameManager.inventoryItem
+@export var water:=5.0
+
+func _process(_delta: float) -> void:
+	$Sprites/WaterControl/Water.position.y=lerp(33.254, 13.615, water/maxWater)
