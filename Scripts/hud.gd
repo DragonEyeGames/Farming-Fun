d580e28027ba9c99650aecbf26a25debd122be08
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-15 12:32:47 -0400

    Watering Meter
    Put in a watering meter that is only visible when the watering can is selected.

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -8,7 +8,15 @@ func _ready() -> void:
 		index+=1
 
 func _process(_delta: float) -> void:
-	$Energy/ProgressBar.value=GameManager.energy
+	$Energy/ProgressBar.value=remap(GameManager.energy, 0, 40, 8.1, 36.6)
+	$Water/ProgressBar.value=remap(GameManager.playerInventory[0].amount, 0, 20, 3.5, 17.5)
+	if((GameManager.selectedItem!=null and str(GameManager.inventoryItem.keys()[GameManager.selectedItem.item])=="Watering_Can")):
+		if($Water.modulate.a==0):
+			var tween=create_tween()
+			tween.tween_property($Water, "modulate:a", 1, .1)
+	elif($Water.modulate.a==1):
+			var tween=create_tween()
+			tween.tween_property($Water, "modulate:a", 0, .1)
 	var total_minutes := int(GameManager.time * 24 * 60)
 	var hours := int(total_minutes / 60.0)
 	var minutes := total_minutes % 60
