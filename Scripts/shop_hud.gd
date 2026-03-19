d08bf794639e88d2d99f9ee8e8789a1ab3cae8b8
Taxalottle Syndicate <bthole41@gmail.com>
2026-03-19 10:57:32 -0400

    Improved Shop HUD
    I made the HUD for the shop better! I fixed up some issues and added in a shell and a close button. The close button doesn't work yet, but almost there.

--- Scripts/shop_hud.gd
+++ Scripts/shop_hud.gd
@@ -12,3 +12,13 @@ func _on_item_pressed(id) -> void:
 		GameManager.addItem(caller.item, 1)
 	else:
 		caller.fail()
+
+
+func _on_close_mouse_entered() -> void:
+	var tween=create_tween()
+	tween.tween_property($Close, "scale", Vector2(.72, .72), .1)
+
+
+func _on_close_mouse_exited() -> void:
+	var tween=create_tween()
+	tween.tween_property($Close, "scale", Vector2(.64, .64), .1)
